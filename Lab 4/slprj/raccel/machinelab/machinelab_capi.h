d6826368d582f0a968248d0ed3d875db63e87bdd
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2020-04-04 15:55:46 +0000

    added Lab 4\slprj\raccel\machinelab\machinelab_capi.h

---
+++ Lab 4/slprj/raccel/machinelab/machinelab_capi.h
@@ -0,0 +1,6 @@
+#include "__cf_machinelab.h"
+#ifndef RTW_HEADER_machinelab_capi_h
+#define RTW_HEADER_machinelab_capi_h
+#include "machinelab.h"
+extern void machinelab_InitializeDataMapInfo ( void ) ;
+#endif
